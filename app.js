e062731714c70aedfe99106ae5259b699578a125
Victor E Gonzalez P <vemiliogp@gmail.com>
2022-03-30 15:05:18 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const { crearArchivo } = require("./helpers/multiplicar");
+const argv = require("./config/yargs");
+require("colors");
+
+console.clear();
+
+// console.log(process.argv);
+// console.log(argv);
+
+// console.log("base: yargs", argv.base);
+
+// const [, , arg3 = "base=5"] = process.argv;
+// const [, base = 5] = arg3.split("=");
+
+// const base = 2;
+
+crearArchivo(argv.b, argv.l, argv.h)
+  .then((nombreArchivo) => console.log(nombreArchivo.rainbow, "creado"))
+  .catch((err) => console.log(err));
